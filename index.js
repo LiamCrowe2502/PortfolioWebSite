3296149cbf70ac017dc6e5943538b5222bd9ba81
LiamCrowe2502 <78029839+LiamCrowe2502@users.noreply.github.com>
2022-03-07 23:25:54 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function toggleMobileMenu(menu) {
     var x = document.getElementById("content");
+    
     if(menu.classList.toggle('open')) {
         x.style.display = "none";
     } else {
         x.style.display = "block";
-      }
+    }
 }
